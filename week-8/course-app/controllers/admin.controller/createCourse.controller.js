d1401c4d650035c5abeda617d5bafc8d38de1708
AnakinCodeWalker <parag31ja@gmail.com>
2025-10-22 23:36:54 +0530

    created auth middleware and course post route

--- week-8/course-app/controllers/admin.controller/createCourse.controller.js
+++ week-8/course-app/controllers/admin.controller/createCourse.controller.js
@@ -1,5 +1,36 @@
+import authAdminMiddleware from "../../middleware/auth.Admin.Middleware"
+import Course from "../../models/course.model"
+
+app.use(authAdminMiddleware)
 const createCourseHandler =async (req,res)=>{
 
-    
+    const adminId=req.userId 
+const {title,description, imageurl, price}=req.body    
+
+if(title||description|| imageurl||price){
+    return res.json({
+        "message":"All details are required"
+    })
 }
+try{
+const course = await Course.create({
+    title,
+    description,
+     imageurl, 
+     price,
+creatorId:adminId
+})
+
+return res.status(200).json({"meesage":"course created successfully",
+    courseId : course.id
+})
+}
+catch(err){
+return res.status(403).json({
+    "message":"can not create the course",
+    err:err.message
+})
+}
+}
+
 export default createCourseHandler
