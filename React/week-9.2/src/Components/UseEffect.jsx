5a4247b2e15482127b38efb860eb18672320feb1
AnakinCodeWalker <parag31ja@gmail.com>
2025-12-27 20:46:19 +0530

    learned about sideEffect , dependency arrray and cleanup code

--- React/week-9.2/src/Components/UseEffect.jsx
+++ React/week-9.2/src/Components/UseEffect.jsx
@@ -1,11 +1,83 @@
-// coding for the UseEffect
-//  useEffect lets you handle sideEffects.
+// Mounting  - means adding the component to the dom [component is rendered frist time]
+// unMounting  - means removing the component from the dom  
 
+// coding for the UseEffect
+
+
+/*  useEffect lets you handle SideEffects.  
+side effects ka matlab are operations that interact with the outside world or have effect beyond the component rendering.
+
+// sideEffect has nothing to do with the component rendering  , something you want to do something on the side they affect the thing outside component rendering.
+
+Api calls 
+Dom manipulation
+setInterval / setTimeout
+Event Listener
+Subscription / cleanup 
+
+
+/* if u add sideEffect direclty in your code componet react will run the code everytime the component re-rerender
+ 
+This leads to 
+in setTimeOut yeh bar bar call hoga  ,bar bar component  rerender hoga.
+
+-> Unnecessary or duplicated effect (multiple Api calls)
+-> Inconsistent behaviour (side effect might happend before rerendering finishes)
+-> performance issue(side effect could block rendering or cause excessive re rendering)
+
+*/
+
+
+/*
+** Subscription means starting a process that listens or runs continuously  , and cleanup means stopping it to avoid memory leaks when the component unmounts or re renders.
+common subscriptions -> setInterval / setTimeout , window.addEventListener , webSocket , firebase listener , API polling  
+*/
+
+
+
+
+import { useState } from "react"
+import { useEffect } from "react"
 
 //  use all 3 show without and with UseEffect cosequences..
 
 function UseEffect(){
-    return <div style={{
+
+
+    const [count , SetCount] = useState(0)
+function setValue(){
+    SetCount(count+1)  //weird behaviour of the code 
+}
+ setInterval(setValue,1000) 
+
+
+//  the useEffect hook 
+ useEffect(() => {
+      
+    console.log(`will run only once as the dependency [] is empty`);
+
+
+    // whatever code will be written inside will run when the  component being mounted [when the component comes for the 1st time]
+
+    //    wrap the sideeffect inside the useEffect  so it will run only one time
+    
+    return () => { //cleanup Code for useEffect.
+           
+          }
+        }, [])  //dependency array{if u want to use a state varaible inside a Useeffect put them into the dependency array}
+
+
+
+useEffect(()=>{
+
+return ()=>{
+console.log(`will re run each time the value of varaible changes ${count}`);
+}
+
+}, [count] ) // logic inside the effect will run each time , as the useState variable changes.
+
+
+ return <div style={{
         backgroundColor:"white",
         padding:10,
         borderRadius:10,
@@ -30,7 +102,10 @@ function UseEffect(){
             borderRadius:50,
             textAlign:"center",
             margin:10
-        }}> hi</div>
+        }}>  {count}
+        without UseEffect Hook
+        </div>
+        
         
         
         <div style={{
@@ -39,7 +114,11 @@ function UseEffect(){
             height:50,
             borderRadius:50,
             textAlign:"center"
-        }}> hi2</div>
+        }}>
+            with UseEffect Hook 
+      
+     
+        {count}</div>
     </div>
     
     
@@ -57,3 +136,12 @@ function UseEffect(){
     </div>
 }
 export default UseEffect
+
+
+
+/*
+cleanup logic even when the code is being un mounted[not present in the dom]
+ the side effects keeps running  
+ Agar cleanup na ho, to sideEffects component remove hone ke baad bhi chalta rehta hai.
+ 
+*/
---
+++ TypeScript/app/notes.js
@@ -0,0 +1,19 @@
+// topics left
+/*
+export and imports
+generics 
+enums
+union / intersection
+arrays 
+*/
+
+// little overwiew on concept of oop
+// A class can implement multiple interfaces.
+// one classs can only extend a single class 
+
+//one interface extends another interface [multiple extension can be done]
+//interface can only have abstract() Interface methods are implicitly abstract — they can’t have implementation (method body). 
+// if a class is implementing an interface or extending an abstract class it can have their field and methods and itw own properties and methods as well
+
+// you can not create an object of abstract classes they can only be extended 
+// abstract classes abstract method must be defined 
