cdc7d1f22dd36d239a950c758c92c0e68677f564
AnakinCodeWalker <parag31ja@gmail.com>
2025-12-28 17:50:39 +0530

    made notes about the useRef

--- React/week-10-useRef'/src/Components/notes.js
+++ React/week-10-useRef'/src/Components/notes.js
@@ -1,4 +1,10 @@
 /*
 useref is a react hook that lets you
-1.
+1.let you store a value that does not cause re render when its value changed[changing values does not cause re render and it persists the value while re renders]
+2.Lets you access the dom directly
+
+3. it returns an mutuable object
+
+4. you always Access/changes the value using 
+ .current  -->  { refrence variable.current }
 */
