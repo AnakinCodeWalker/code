d3e71c416d106266e761099d73311bc357bcc3fa
AnakinCodeWalker <parag31ja@gmail.com>
2025-12-31 16:14:02 +0530

    done with the contextApi

--- React/week-10.2-ContextApi/src/component/ContextApi.jsx
+++ React/week-10.2-ContextApi/src/component/ContextApi.jsx
@@ -1,14 +1,46 @@
-import { createContext } from "react"
+
+import { useContext} from "react"
+import BulbContext from "./BulbContext.js"
 /*
 1. createContext()
 2.provider
 3.consumer
 */
-const bublContext = createContext()
+
+
+// created the contex
+// wrapped the component around the context and then .Provided value={{
+//  values u want to pass in the object
+// }}
+// and the children can use the context/ object via useContext().
 const ContextApi = () => {
+ 
+  
     return (
-        <div>ContextApi</div>
+        <div>ContextApi
+            <LightBulb/>
+            <LightSwitch/>
+        </div>
     )
+}
+
+function LightBulb(){
+    const {bulb} = useContext(BulbContext)
+    return <div>
+{bulb}
+
+    </div>
+}
+
+function LightSwitch(){
+  const {bulb ,setBulb} = useContext(BulbContext)
+    return <div>
+        {bulb}
+
+        <button onClick={function(){
+            setBulb(b=>!b)
+        }}></button>
+    </div>
 }
 
 export default ContextApi
