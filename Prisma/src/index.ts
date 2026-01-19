f4bdf0e410f682419c11d918744101ff054639e4
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-19 23:49:54 +0530

    created model in prisma

--- Prisma/src/index.ts
+++ Prisma/src/index.ts
@@ -1,1 +1,33 @@
-console.log("Prisma setup starting");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+
+interface detials {
+       username: string,
+    password: string,
+    firstName: string,
+    email: string
+}
+async function main(detials:detials) {
+    const user =await prisma.user.create({
+        // @ts-ignore
+        data: {
+     username: detials.username,
+    password: detials.password,
+    firstName: detials.firstName,
+    email: detials.email
+    }
+    })
+    console.log(user);
+}
+
+
+
+main({
+  username: "prince",
+  password: "123",
+  firstName: "Prince",
+  email: "prince@gmail.com",
+})    
+
