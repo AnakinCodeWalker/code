195ffe87dd0ccd6add641c0d266e9a265c1903f0
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-24 23:49:56 +0530

    websSocket with typeScript and React

--- Socket/Backend/src/index.ts
+++ Socket/Backend/src/index.ts
@@ -1,25 +1,25 @@
-import {WebSocketServer} from "ws"
+import {WebSocketServer ,WebSocket} from "ws"
 
 //  this does not accepts a cb ..
 const wss = new WebSocketServer({port: 3000})
 
 
 // wss is for the server  , ws is for a single client..
-wss.on("connection",(ws)=>{
+wss.on("connection",(Socket : WebSocket)=>{
     console.log(`connected to the client`);
 
 
-ws.on("message",(msg)=>{
+Socket.on("message",(msg : JSON | string)=>{
  const data = JSON.parse(msg.toString());
     console.log(data);
 
 
-ws.send("hello from server")
+Socket.send("hello from server")
 })
 
 
 
-ws.on("close", () => {
+Socket.on("close", () => {
     console.log("Client disconnected");
   });
 
@@ -28,12 +28,18 @@ ws.on("close", () => {
 
 
 
-// is a web protocol like http
+// is a web protocol like http ,full duplex communication.. uses tcp 
 // scaling them is very tough compared to http one
 
-// http request uses 3 way handshake it close the connection  when u got the response they are not presistent you have to create  a connection again when u want to send a request
+// http request uses 3 way handshake it close the connection  when u got the response they are not presistent you have to create  a connection again when u want to send a request it also uses tcp 
 
 // while websocket and webrtc is a persistent connection also a 3 way hand shake protocol one time a connection is made it stays there.
+
+// for real time communications ws  is used.
+
+//  because ws is able to perform  2 way communication , that is why we use it in chat  application here server can send it.
+
+// u could do the same with http but u will need long polling. clients can keep polling
 
 
 
