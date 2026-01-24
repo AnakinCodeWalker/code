8125e12795a88279a46bcb4998e72144453d2328
AnakinCodeWalker <parag31ja@gmail.com>
2026-01-25 00:11:44 +0530

    Created the simple frontend

---
+++ Socket/Frontend/src/Components/Button.tsx
@@ -0,0 +1,28 @@
+
+const ButtonComponent = () => {
+    return (
+        <>
+            <div>
+                <label style={{
+                    marginRight: 10
+                }} htmlFor="name">Enter ping</label>
+
+                <input style={{
+                    marginBottom: 10
+                }} type="text" />
+
+
+            </div>
+            <button style={{
+                backgroundColor: "#aff",
+                borderRadius: 10,
+                marginTop: 5,
+                padding: 7
+            }} onClick={function () {
+
+            }}>Click me</button>
+        </>
+    )
+}
+
+export default ButtonComponent
