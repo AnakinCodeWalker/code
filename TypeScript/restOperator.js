72b4c2a7101942697e4c0c106c99a713a65debec
AnakinCodeWalker <parag31ja@gmail.com>
2026-02-10 23:33:28 +0530

    added more or restOperator in js

--- TypeScript/restOperator.js
+++ TypeScript/restOperator.js
@@ -7,14 +7,32 @@ console.log(`${arr}
 
 const obj1 ={
      name : "kutta"
+
 }
 
 const obj2 = {
-     ...obj1,
-     title : "kumar"
+     ...obj1,  //copying everything from the object 1 to object 2 
+ caste : "kumar",              // adding its own properties
 }
-
 console.log(obj1);
 console.log("using rest operator ");
 
 console.log(obj2);
+
+console.log(" ");
+// overriding the properties and using rest operator
+
+const oldObject ={
+     name : "kutta",
+title :"kumar kabir"
+}
+
+const newObject={
+     ...oldObject,  //what this will do is copy everything from object 1  to object 2
+title : "santa" // and the property of object 1 will be overriden .. here
+}
+
+console.log(oldObject);
+
+console.log("using the rest operator");
+console.log(newObject);
